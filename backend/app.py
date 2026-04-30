18001ac847ca32c88289ab9b8899d04b07c6f66f
Pavithra <mayindragopalakrishnan@gmail.com>
2026-05-01 01:20:25 +0530

    Updated app.py for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -191,4 +191,5 @@ def download_file(file_id):
 # ---------------- RUN ----------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
